892b3cf92e8489b6e9ac41f19fe3c4cda9530b2f
mzhang001 <zhangmeng1010@gmail.com>
2016-01-31 10:26:50 +0000

    Add start page for clicking

--- drumbackapp/urls.py
+++ drumbackapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^start$', TemplateView.as_view(template_name='start.html'), name='start'),
     url(r'^drum', TemplateView.as_view(template_name='drum.html'), name='drum'),
     url(r'^index', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'^thanks', TemplateView.as_view(template_name='thanks.html'), name='thanks')
+    url(r'^thanks', TemplateView.as_view(template_name='thanks.html'), name='thanks'),
+    url(r'^challengemode', TemplateView.as_view(template_name='challengemode.html'), name='challengemode')
 ]
